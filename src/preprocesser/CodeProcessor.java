c06379d27ad56f7c966a8d334d8c22565a3b231c
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-03-17 04:00:00 +0100

    Fix: remove comments; Feature: Main for debugging (remove before merging)

--- src/preprocesser/CodeProcessor.java
+++ src/preprocesser/CodeProcessor.java
@@ -1,8 +1,11 @@
 package preprocesser;
 
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CodeProcessor implements LineRequest {
+    private static final Pattern comment = Pattern.compile("^([^\"/]|(?<=/)?/(?>/)?)*(?:\"(?:[^\"]|(?:(?<=\\\\)\"))*\"[^\"/]*)*//"); // detect comments outside text
     private final FileManager fm;
 
     public CodeProcessor(String path) throws FileNotFoundException {
@@ -10,6 +13,16 @@ public class CodeProcessor implements LineRequest {
     }
 
     public String getNextLine() {
-        return this.fm.readNextLine().replaceAll("//.*$", ""); // TODO si añadimos string hay que comprobar que no haya '//' en literal
+        String next = this.fm.readNextLine();
+        Matcher match = CodeProcessor.comment.matcher(next);
+        if (match.find()) next = next.substring(0, match.end()-2);
+        return next;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        CodeProcessor cp = new CodeProcessor("test.sus");
+        System.out.println(cp.getNextLine());
+        System.out.println(cp.getNextLine());
+        System.out.println(cp.getNextLine());
     }
 }
