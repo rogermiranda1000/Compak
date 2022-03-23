33317e56b7725ab7c62250cf9e16c7f07468f86e
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 15:02:30 +0100

    LDPG22122-106 Finalitzat la estructuració de la gramatica de les operacions matematiques.

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -13,9 +13,10 @@ public class GrammarAnalizer implements GrammarRequest {
     private static Production n1;
     private static Production n1Sub;
     private static Production n2;
+    private static Production n2Sub;
     private static Production n3;
     private static Production n3Sub;
-    private static Production n4;
+    private static Production negat;
     private static Production n5;
     private static Production ID;
     private static Production symbolSumaResta;
@@ -46,8 +47,48 @@ public class GrammarAnalizer implements GrammarRequest {
         );
         n1Sub = new Production(
                 new Object[]{symbolSumaResta, n2, n1Sub},
-                new Object[]{},
-                new Object[]{symbolDivMult, n3, n2S}
+                new Object[]{}
+        );
+        n2 = new Production(
+                new Object[]{n3, n2Sub}
+        );
+        n2Sub = new Production(
+                new Object[]{symbolDivMult, n3, n2Sub},
+                new Object[]{}
+        );
+        n3 = new Production(
+                new Object[]{n5, n3Sub}
+        );
+        n3Sub = new Production(
+                new Object[]{Token.RAISE, n5, n3Sub},
+                new Object[]{}
+        );
+        negat = new Production(
+                new Object[]{Token.NOT},
+                new Object[]{}
+        );
+        n5 = new Production(
+                new Object[]{negat, Token.OPN_PARENTH, n0, Token.CLS_PARENTH},
+                new Object[]{ID}
+        );
+        ID = new Production(
+                new Object[]{id}
+        );
+        symbolSumaResta = new Production(
+                new Object[]{Token.SUM},
+                new Object[]{Token.SUBSTRACT}
+        );
+        symbolDivMult = new Production(
+                new Object[]{Token.MULT},
+                new Object[]{Token.DIVIDE},
+                new Object[]{Token.MOD}
+        );
+        symbolCond = new Production(
+                new Object[]{Token.OR},
+                new Object[]{Token.AND},
+                new Object[]{Token.COMP},
+                new Object[]{Token.LT},
+                new Object[]{Token.GT}
         );
     }
 
