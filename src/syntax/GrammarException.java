dbaa4e1a28b0ed8f6c17cd515662c48242cc817d
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 01:52:22 +0100

    LDPG22122-98 Finalitzada l'estructura del start de la gramatica

--- src/syntax/GrammarException.java
+++ src/syntax/GrammarException.java
@@ -2,6 +2,6 @@ package syntax;
 
 public class GrammarException extends Exception {
     public GrammarException(String printInfo) {
-        System.out.println(printInfo);
+        super(printInfo);
     }
 }
