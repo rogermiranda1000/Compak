a890efc80ddfb3de2e33e0a10a81a1989f31e08d
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 02:24:51 +0100

    LDPG22122-102 Finalitzada l'estructura dels condicionals

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -6,6 +6,9 @@ import entities.Token;
  * Aquesta classe contindrà totes les produccions de Gramàtica
  */
 public class GrammarAnalizer implements GrammarRequest {
+    private static Production condicional;
+    private static Production elseConditional;
+
     private final Production value_bit = new Production(
             new Object[]{Token.TRUE},
             new Object[]{Token.FALSE}
@@ -18,7 +21,13 @@ public class GrammarAnalizer implements GrammarRequest {
     );
 
     static {
-
+        condicional = new Production(
+                new Object[]{Token.IF, Token.OPN_PARENTH, n0, Token.CLS_PARENTH, elseConditional}
+        );
+        elseConditional = new Production(
+                new Object[]{Token.ELSE, Token.OPN_CONTEXT, sentencies, Token.CLS_CONTEXT},
+                new Object[]{}
+        );
     }
 
     public GrammarAnalizer() {}
