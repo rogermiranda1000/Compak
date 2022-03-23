8d23f298dd73c24d76d14d870d8a215023572388
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 02:09:04 +0100

    LDPG22122-100 Finalitzada l'estructura de les funcions

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -6,6 +6,11 @@ import entities.Token;
  * Aquesta classe contindrà totes les produccions de Gramàtica
  */
 public class GrammarAnalizer implements GrammarRequest {
+    private static Production declaracioFuncio;
+    private static Production declaracioFuncioSub;
+    private static Production arguments;
+    private static Production argumentsSub;
+
     private final Production value_bit = new Production(
             new Object[]{Token.TRUE},
             new Object[]{Token.FALSE}
@@ -18,6 +23,20 @@ public class GrammarAnalizer implements GrammarRequest {
     );
 
     static {
+        declaracioFuncio = new Production(
+                new Object[]{Token.FUNC, nomFuncio, Token.OPN_PARENTH, arguments, Token.CLS_PARENTH, declaracioFuncioSub, Token.OPN_CONTEXT, sentencies, Token.CLS_CONTEXT}
+        );
+        declaracioFuncioSub = new Production(
+                new Object[]{Token.RET_TYPE, tipus},
+                new Object[]{}
+        );
+        arguments = new Production(
+                new Object[]{tipus, nomVariable, argumentsSub}
+        );
+        argumentsSub = new Production(
+                new Object[]{Token.COMMA, arguments},
+                new Object[]{}
+        );
 
     }
 
