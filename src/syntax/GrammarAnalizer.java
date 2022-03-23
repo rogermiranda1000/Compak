27426743f12af3a3eeac7df737acfdd0306293b8
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 02:19:22 +0100

    LDPG22122-101 Finalitzada l'estructura dels bucles

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -6,6 +6,8 @@ import entities.Token;
  * Aquesta classe contindrà totes les produccions de Gramàtica
  */
 public class GrammarAnalizer implements GrammarRequest {
+    private static Production declaracioBucle;
+    private static Production declaracioBucleSub;
     private final Production value_bit = new Production(
             new Object[]{Token.TRUE},
             new Object[]{Token.FALSE}
@@ -18,7 +20,13 @@ public class GrammarAnalizer implements GrammarRequest {
     );
 
     static {
-
+        declaracioBucle = new Production(
+                new Object[]{Token.BUCLE, Token.OPN_PARENTH, declaracioBucleSub, Token.CLS_PARENTH, Token.OPN_CONTEXT, sentencies, Token.CLS_CONTEXT}
+        );
+        declaracioBucleSub = new Production(
+                new Object[]{n0},
+                new Object[]{Token.FOR, nomVariable, Token.IN, Token.RANGE, Token.OPN_PARENTH, valueNumber, Token.CLS_PARENTH}
+        );
     }
 
     public GrammarAnalizer() {}
