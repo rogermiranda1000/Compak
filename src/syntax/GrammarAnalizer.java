7b32fad63f9ed8ff0fc3040b48118212c1e92f45
guillem.serra <guillem.serra@students.salle.url.edu>
2022-03-23 15:26:26 +0100

    LDPG22122-107 Finalitzat la estructuració de la gramatica dels IDs

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -6,6 +6,14 @@ import entities.Token;
  * Aquesta classe contindrà totes les produccions de Gramàtica
  */
 public class GrammarAnalizer implements GrammarRequest {
+    private static Production id;
+    private static Production idStr;
+    private static Production idNumOrFloat;
+    private static Production idCond;
+    private static Production digit;
+    private static Production letter;
+    private static Production anyCharacter;
+
     private final Production value_bit = new Production(
             new Object[]{Token.TRUE},
             new Object[]{Token.FALSE}
@@ -18,7 +26,26 @@ public class GrammarAnalizer implements GrammarRequest {
     );
 
     static {
-
+        id = new Production(
+                new Object[]{value},
+                new Object[]{nomVariable}
+        );
+        idStr = new Production(
+                new Object[]{valueString},
+                new Object[]{nomVariable}
+        );
+        idNumOrFloat = new Production(
+                //TODO: Fer el REGEX al enum
+                new Object[]{valueNumber, possibleFloat},
+                new Object[]{nomVariable}
+        );
+        idCond = new Production(
+                new Object[]{valueBit},
+                new Object[]{nomVariable}
+        );
+        digit = new Production(
+                new Object[]{Token.NUMBER}
+        );
     }
 
     public GrammarAnalizer() {}
