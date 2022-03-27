098917cd89aa70552583cdb4054f0c86e207d8c6
roger.miranda <roger.miranda@students.salle.url.edu>
2022-03-27 18:13:49 +0200

    Fix: null productions Feature: first&follow 1/2

--- src/syntax/GrammarRequest.java
+++ src/syntax/GrammarRequest.java
@@ -1,5 +1,22 @@
 package syntax;
 
-public interface GrammarRequest {
-    public Production getEntryPoint();
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class GrammarRequest {
+    public abstract Production getEntryPoint();
+
+    public Map<String, Production> getProductions() {
+        HashMap<String, Production> r = new HashMap<>();
+        try {
+            for (Field f : this.getClass().getDeclaredFields()) {
+                if (!(f.getType().equals(Production.class))) continue;
+                r.put(f.getName(), (Production)f.get(this));
+            }
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        }
+        return r;
+    }
 }
--- src/syntax/Production.java
+++ src/syntax/Production.java
@@ -2,26 +2,40 @@ package syntax;
 
 import entities.Token;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Production {
     /**
      * Producció nul·la
      */
     public static final Production EPSILON = new Production();
 
-    private final Object [][]produccions;
+    private final List<Object[]> produccions;
+
+    private static void checkArgument(Object[] production) throws IllegalArgumentException {
+        for (Object o : production) {
+            if (!(o instanceof Production || o instanceof Token)) throw new IllegalArgumentException("L'array només pot contenir produccions i tokens! (S'ha rebut " + (o == null ? "null" : o.getClass().toString()) + ")");
+        }
+    }
 
     /**
      *
      * @param productions S'han de passar n (sent n el número d'ors de la producció) arrays que continguin produccions o tokens
      */
     public Production(Object[] ...productions) {
+        this.produccions = new ArrayList<>();
+
         // check arguments
         for (Object[] p : productions) {
-            for (Object o : p) {
-                if (!(o instanceof Production || o instanceof Token)) throw new IllegalArgumentException("L'array només pot contenir produccions i tokens!");
-            }
+            Production.checkArgument(p);
+            this.produccions.add(p);
         }
+    }
 
-        this.produccions = productions;
+    public Production addProduction(Object ...production) {
+        Production.checkArgument(production);
+        this.produccions.add(production);
+        return this;
     }
 }
