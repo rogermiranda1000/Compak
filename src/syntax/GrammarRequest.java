4f7981dafe0ec8195fac122efcd0407d3ed0360a
roger.miranda <roger.miranda@students.salle.url.edu>
2022-03-27 19:39:31 +0200

    Feature: follow 1/2

--- src/syntax/GrammarRequest.java
+++ src/syntax/GrammarRequest.java
@@ -3,10 +3,7 @@ package syntax;
 import entities.Token;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class GrammarRequest {
     public abstract Production getEntryPoint();
@@ -24,8 +21,8 @@ public abstract class GrammarRequest {
         return r;
     }
 
-    private static List<Token> getFirst(Production p) {
-        List<Token> r = new ArrayList<>();
+    private Set<Token> getFirst(Production p) {
+        Set<Token> r = new HashSet<>();
 
         for (Object[] production : p.getProduccions()) {
             /**
@@ -33,8 +30,8 @@ public abstract class GrammarRequest {
              * If x-> ε, is a production rule, then add ε to FIRST(x).
              * If X->Y1 Y2 Y3….Yn is a production,
              *      FIRST(X) = FIRST(Y1)
-             *      If FIRST(Y1) contains Є then FIRST(X) = { FIRST(Y1) – Є } U { FIRST(Y2) }
-             *      If FIRST (Yi) contains Є for all i = 1 to n, then add Є to FIRST(X).
+             *      If FIRST(Y1) contains ε then FIRST(X) = { FIRST(Y1) – ε } U { FIRST(Y2) }
+             *      If FIRST (Yi) contains ε for all i = 1 to n, then add Є to FIRST(X).
              *
              * @author https://www.geeksforgeeks.org/first-set-in-syntax-analysis/
              */
@@ -44,28 +41,63 @@ public abstract class GrammarRequest {
             else {
                 boolean found_epsilon = true;
                 for (int x = 0; x < production.length && found_epsilon /* if FIRST(Y1) contains Є then FIRST(X) = { FIRST(Y1) – Є } U { FIRST(Y2) } */; x++) {
-                    List<Token> next = GrammarRequest.getFirst((Production) production[x]);
+                    Set<Token> next = this.getFirst((Production) production[x]);
                     if (!next.remove(Token.EPSILON)) {
                         found_epsilon = false; // no hay epsilon
                     }
                     r.addAll(next);
                 }
-                if (found_epsilon) r.add(Token.EPSILON); // FIRST (Yi) contains Є for all i = 1 to n
+                if (found_epsilon) r.add(Token.EPSILON); // FIRST (Yi) contains ε for all i = 1 to n
             }
         }
 
         return r;
     }
 
-    private static List<Token> getFollow(Production p) {
-        return null;
+    private Set<Token> getFirst(Object p) {
+        if (p instanceof Production) return this.getFirst((Production) p);
+
+        // si no és una producció, és un token
+        Set<Token> r = new HashSet<>();
+        r.add((Token) p);
+        return r;
+    }
+
+    private Map<Production, Set<Token>> getFollows() {
+        Map<Production, Set<Token>> r = new HashMap<>();
+        for (Map.Entry<String, Production> p : this.getProductions().entrySet()) r.put(p.getValue(), new HashSet<>());
+
+        for (Map.Entry<String, Production> _p : this.getProductions().entrySet()) {
+            Production production = _p.getValue();
+
+            /**
+             * The starting production contains the EOF: FOLLOW(S) = { $ }
+             * If A -> pBq is a production (where p and q can be a whole string), then FOLLOW(B) = {FIRST(q) - ε}
+             * If A->pB is a production, then FOLLOW(B) = FOLLOW(A)
+             * If A->pBq is a production and FIRST(q) contains ε, then FOLLOW(B) = { FIRST(q) – ε } U FOLLOW(A)
+             */
+
+            if (production.equals(this.getEntryPoint())) r.get(production).add(Token.EOF); // the starting production contains the EOF
+            for (Object[] p : production.getProduccions()) {
+                for (int x = 0; x < p.length - 1 /* tiene que tener algo delante */; x++) {
+                    if (p[x] instanceof Production) {
+                        Set<Token> first = this.getFirst(p[x+1]);
+                        first.remove(Token.EPSILON);
+                        r.get((Production) p[x]).addAll(first); // if A -> pBq is a production, then FOLLOW(B) = {FIRST(q) - ε}
+                    }
+                }
+            }
+        }
+
+        return r;
     }
 
     public List<FirstFollowData> getFirstFollow() {
         List<FirstFollowData> r = new ArrayList<>();
+        Map<Production, Set<Token>> follows = this.getFollows();
         for (Map.Entry<String, Production> e : this.getProductions().entrySet()) {
             Production p = e.getValue();
-            r.add(new FirstFollowData(e.getKey(), p, GrammarRequest.getFirst(p), GrammarRequest.getFollow(p)));
+            r.add(new FirstFollowData(e.getKey(), p, this.getFirst(p), follows.get(p)));
         }
         return r;
     }
