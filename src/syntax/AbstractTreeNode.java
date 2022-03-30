6c35dae7d0b03ceb1ebb06243185622d18871e5f
roger.miranda <roger.miranda@students.salle.url.edu>
2022-03-30 23:04:14 +0200

    Feature: SymbolTable

---
+++ src/syntax/AbstractTreeNode.java
@@ -0,0 +1,10 @@
+package syntax;
+
+public class AbstractTreeNode {
+    private Production originalProduction;
+
+    /**
+     * Array of AbstractTreeNode or Tokens
+     */
+    private Object[] treeExtend;
+}
---
+++ src/syntax/Compiler.java
@@ -0,0 +1,7 @@
+package syntax;
+
+import java.io.File;
+
+public interface Compiler {
+    public void compile(File out);
+}
