0a64372b36465bae9ae2a027e309c2ed5dbfc298
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-03-31 16:58:35 +0200

    Feature: Tree generator and validator

--- src/lexic/TokenRequest.java
+++ src/lexic/TokenRequest.java
@@ -2,8 +2,11 @@ package lexic;
 
 import entities.TokenDataPair;
 
+import java.util.List;
+
 public interface TokenRequest {
     public TokenDataPair requestNextToken();
     public int getCurrentLine();
     public int getCurrentColumn();
+    public void returnTokens(List<TokenDataPair> tokens);
 }
