8da4d5fc513d5defa4d951370d3ff9a65c8cc55b
Pol <pol.pinol@students.salle.url.edu>
2022-04-07 01:33:03 +0200

    Fixed bug at production "negat" where always entered as being opened the possibility of the first token be null.
    New example to text on file.sus.

--- src/syntax/GrammarAnalizer.java
+++ src/syntax/GrammarAnalizer.java
@@ -42,49 +42,49 @@ public class GrammarAnalizer extends GrammarRequest {
     );
 
     protected static final Production tipus = new Production(
-        new Object[]{Token.STR},
-        new Object[]{Token.BIG},
-        new Object[]{Token.INT},
-        new Object[]{Token.BIT},
-        new Object[]{Token.FLO}
+            new Object[]{Token.STR},
+            new Object[]{Token.BIG},
+            new Object[]{Token.INT},
+            new Object[]{Token.BIT},
+            new Object[]{Token.FLO}
     );
 
     protected static final Production declaracioFuncioSub = new Production(
-        new Object[]{Token.RET_TYPE, tipus},
-        new Object[]{}
+            new Object[]{Token.RET_TYPE, tipus},
+            new Object[]{}
     );
 
     protected static final Production declaracioVariable = new Production(
-        new Object[]{tipus, Token.ID}
+            new Object[]{tipus, Token.ID}
     );
 
     protected static final Production symbolSumaResta = new Production(
-        new Object[]{Token.SUM},
-        new Object[]{Token.SUBSTRACT}
+            new Object[]{Token.SUM},
+            new Object[]{Token.SUBSTRACT}
     );
 
     protected static final Production symbolDivMult = new Production(
-        new Object[]{Token.MULT},
-        new Object[]{Token.DIVIDE},
-        new Object[]{Token.MOD}
+            new Object[]{Token.MULT},
+            new Object[]{Token.DIVIDE},
+            new Object[]{Token.MOD}
     );
 
     protected static final Production symbolCond = new Production(
-        new Object[]{Token.OR},
-        new Object[]{Token.AND},
-        new Object[]{Token.COMP},
-        new Object[]{Token.LT},
-        new Object[]{Token.GT}
+            new Object[]{Token.OR},
+            new Object[]{Token.AND},
+            new Object[]{Token.COMP},
+            new Object[]{Token.LT},
+            new Object[]{Token.GT}
     );
 
     protected static final Production negat = new Production(
-        new Object[]{Token.NOT},
-        new Object[]{}
+            new Object[]{Token.NOT},
+            new Object[]{}
     );
 
     protected static final Production id = new Production(
-        new Object[]{value},
-        new Object[]{Token.ID}
+            new Object[]{value},
+            new Object[]{Token.ID}
     );
 
     static {
@@ -133,8 +133,9 @@ public class GrammarAnalizer extends GrammarRequest {
         n3Sub.addProduction(Token.RAISE, n5, n3Sub)
                 .addProduction();
 
-        n5.addProduction(negat, Token.OPN_PARENTH, n0, Token.CLS_PARENTH)
-                .addProduction(Token.ID);
+        n5.addProduction(Token.NOT, Token.OPN_PARENTH, n0, Token.CLS_PARENTH)
+                .addProduction(Token.OPN_PARENTH, n0, Token.CLS_PARENTH)
+                .addProduction(id);
 
         sentencies.addProduction(possibleSentencies, sentencies)
                 .addProduction(Token.RETURN, id, Token.EOL)
