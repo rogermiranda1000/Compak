f5c7ba29a55035b83df5691b892802e4dffac11c
Pol Piñol Castuera <pol.pinolcastuera@gmail.com>
2022-04-11 01:13:12 +0200

    Added removeMeaningLessTokens function that removes all tokens without meaning in syntax.
    Started function that promotes important tokens as =, +, *, etc (operational tokens)

--- src/syntax/AbstractSyntaxTree.java
+++ src/syntax/AbstractSyntaxTree.java
@@ -1,11 +1,18 @@
 package syntax;
 
+import entities.Token;
 import entities.TokenDataPair;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class AbstractSyntaxTree extends Tree {
+    // Remove nodes for tokens that don't add meaning. Those are intermediate keywords (like "then"), separators (like comma) and brackets (like parenthesis).
+    // Promote meaningful tokens (like "if") to be the parent of other tokens in the same rule.
+    Token operation = null;
+
     public AbstractSyntaxTree(ParseTree parseTree) {
         super();
         cloneTree(parseTree);
@@ -53,5 +60,44 @@ public class AbstractSyntaxTree extends Tree {
                 }
             }
         }
+    }
+
+    public void removeMeaningLessTokens() {
+        Token[] list = new Token[] {Token.EOL, Token.INT, Token.BIG,Token.FLO,Token.STR,Token.BIT,Token.MAIN,Token.RET_TYPE,Token.IN,
+                Token.RANGE,Token.OPN_CONTEXT,Token.CLS_CONTEXT,Token.OPN_PARENTH,Token.CLS_PARENTH,Token.COMMA,Token.FOR};
+
+        for (int i = 0; i < this.treeExtend.size(); i++) {
+            Object o = this.treeExtend.get(i);
+
+            if (o instanceof AbstractSyntaxTree) {
+                ((AbstractSyntaxTree)o).removeMeaningLessTokens();
+            } else {
+                if (((TokenDataPair)o).isMeaningLessToken(list)) {
+                    this.treeExtend.remove(o);
+                    i--;
+                }
+            }
+        }
+    }
+
+    public void promoteTokens() {
+        Queue<TokenDataPair> queue = new LinkedList<TokenDataPair>();
+
+        for (int i = 0; i < this.treeExtend.size(); i++) {
+            Object o = this.treeExtend.get(i);
+
+            if (o instanceof AbstractSyntaxTree) {
+                if (((AbstractSyntaxTree)o).treeExtend.get(0) instanceof TokenDataPair) {
+                    Token token = ((TokenDataPair) ((AbstractSyntaxTree)o).treeExtend.get(0)).getToken();
+                    if (token == Token.ASSIGN) {
+
+                    }
+                }
+                ((AbstractSyntaxTree)o).promoteTokens();
+            } else {
+            }
+        }
+
+
     }
 }
