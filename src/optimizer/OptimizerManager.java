4a7dd29795eb35b42fcc1c64216a2f441d93167b
Pol Piñol Castuera <pol.pinol@students.salle.url.edu>
2022-05-03 16:04:09 +0200

    Swap and remove optimization tried.

--- src/optimizer/OptimizerManager.java
+++ src/optimizer/OptimizerManager.java
@@ -1,11 +1,8 @@
 package optimizer;
 
-import entities.Tag;
-
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Stack;
 
 public class OptimizerManager {
     private ArrayList<String> lines;
@@ -20,8 +17,95 @@ public class OptimizerManager {
 
     public void optimize() {
         getTACLines();
+        //optimizeRedundantAssigns();
         refactorVarsNames();
         writeTACtoFile();
+    }
+
+    // @TODO finish swap and remove
+    private void optimizeRedundantAssigns() {
+        for (int i = 0; i < lines.size(); i++) {
+            // Search for case t0 := t1
+            String[] parts = lines.get(i).split(" ");
+            if (parts.length == 3 && parts[0].matches("^(\\d*[a-zA-Z_]+\\w*)$") &&
+                    parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")
+                        && !parts[2].equals("true") && !parts[2].equals("false")) {
+                // look for t1 and substitute all
+                swapAndRemove(parts[2], parts[0], i);
+            }
+        }
+    }
+
+    // @TODO finish swap and remove
+    private void swapAndRemove(String b, String a, int start) {
+        for (int i = start; i < lines.size(); i++) {
+            String[] parts = lines.get(i).split(" ");
+
+            if (parts.length == 3) {
+                // case t0 := t1
+                if (parts[0].equals(a)) {
+                    parts[0] = b;
+                }
+
+                if (parts[2].equals(a)) {
+                    parts[2] = b;
+                }
+
+                if (parts[0].equals(parts[2])) {
+                    // Case remove
+                    // lines.remove(i);
+                    // i--;
+                } else {
+                    lines.set(i, parts[0] + " " + parts[1] + " " + parts[2]);
+                }
+            } else if (parts.length == 5) {
+                if (lines.get(i).contains("goto")) {
+                    if (lines.get(i).contains("!")) {
+                        // L0: if !t3 goto L1
+                        if (parts[2].equals("!" + a)) {
+                            parts[2] = "!" + b;
+                        }
+                        lines.set(i, parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + parts[4]);
+                    } else {
+                        // L0: if t3 goto L1
+                        if (parts[2].equals(a)) {
+                            parts[2] = b;
+                        }
+                        lines.set(i, parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + parts[4]);
+                    }
+                } else {
+                    // case t0 := t1 + t2
+                    if (parts[0].equals(a)) {
+                        parts[0] = b;
+                    }
+
+                    if (parts[2].equals(a)) {
+                        parts[2] = b;
+                    }
+
+                    if (parts[4].equals(a)) {
+                        parts[4] = b;
+                    }
+                    lines.set(i, parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + parts[4]);
+                }
+            } else if (parts.length == 2) {
+                // case goto L0
+            } else if (parts.length == 1) {
+                // case L0:
+            } else if (parts.length == 7) {
+                if (parts[2].equals(a)) {
+                    parts[2] = b;
+                }
+
+                if (parts[4].equals(a)) {
+                    parts[4] = b;
+                }
+
+                data.add(parts[0] + " " + parts[1] + " " + parts[2] + " " + parts[3] + " " + parts[4] + " " + parts[5] + " " + parts[6]);
+            } else {
+                System.out.println("ERROR_B");
+            }
+        }
     }
 
     private void refactorVarsNames() {
@@ -47,7 +131,7 @@ public class OptimizerManager {
                 }
 
                 String t1;
-                if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")) {
+                if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[2].equals("true") && !parts[2].equals("false")) {
                     // is variable
                     if (map.containsKey(parts[2])) {
                         t1 = map.get(parts[2]);
@@ -67,7 +151,7 @@ public class OptimizerManager {
                     if (lines.get(i).contains("!")) {
                         // L0: if !t3 goto L1
                         String t0;
-                        if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")) {
+                        if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[2].equals("true") && !parts[2].equals("false")) {
                             // is variable
                             if (map.containsKey(parts[2].replace("!", ""))) {
                                 t0 = map.get(parts[2].replace("!", ""));
@@ -84,7 +168,7 @@ public class OptimizerManager {
                     } else {
                         // L0: if t3 goto L1
                         String t0;
-                        if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")) {
+                        if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[2].equals("true") && !parts[2].equals("false")) {
                             // is variable
                             if (map.containsKey(parts[2])) {
                                 t0 = map.get(parts[2]);
@@ -117,7 +201,7 @@ public class OptimizerManager {
                     }
 
                     String t1;
-                    if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")) {
+                    if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[2].equals("true") && !parts[2].equals("false")) {
                         // is variable
                         if (map.containsKey(parts[2])) {
                             t1 = map.get(parts[2]);
@@ -132,7 +216,7 @@ public class OptimizerManager {
                     }
 
                     String t2;
-                    if (parts[4].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[0].equals("true") && !parts[0].equals("false")) {
+                    if (parts[4].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[4].equals("true") && !parts[4].equals("false")) {
                         // is variable
                         if (map.containsKey(parts[4])) {
                             t2 = map.get(parts[4]);
@@ -155,8 +239,41 @@ public class OptimizerManager {
             } else if (parts.length == 1) {
                 // case L0:
                 data.add(lines.get(i));
+            } else if (parts.length == 7) {
+                // case L0: if t0 >= t1 goto L2
+                String t0;
+                if (parts[2].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[2].equals("true") && !parts[2].equals("false")) {
+                    // is variable
+                    if (map.containsKey(parts[2])) {
+                        t0 = map.get(parts[2]);
+                    } else {
+                        t0 = "t" + index;
+                        map.put(parts[2], t0);
+                        index++;
+                    }
+                } else {
+                    // is number
+                    t0 = parts[2];
+                }
+
+                String t1;
+                if (parts[4].matches("^(\\d*[a-zA-Z_]+\\w*)$") && !parts[4].equals("true") && !parts[4].equals("false")) {
+                    // is variable
+                    if (map.containsKey(parts[4])) {
+                        t1 = map.get(parts[4]);
+                    } else {
+                        t1 = "t" + index;
+                        map.put(parts[4], t1);
+                        index++;
+                    }
+                } else {
+                    // is number
+                    t1 = parts[4];
+                }
+
+                data.add(parts[0] + " " + parts[1] + " " + t0 + " " + parts[3] + " " + t1 + " " + parts[5] + " " + parts[6]);
             } else {
-                data.add("ERROR");
+                data.add("ERROR_A");
             }
         }
     }
