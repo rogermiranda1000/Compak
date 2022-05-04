37aa1eff4db0bd02b8b7cb8853586923dc0f6fdc
roger.miranda <roger.miranda@students.salle.url.edu>
2022-05-05 01:51:09 +0200

    Feature: column number Fix: row number while returning tokens

---
+++ src/entities/TokenPosition.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class TokenPosition {
+    private final int line, column;
+    public TokenPosition(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+}
