b57d33b8d19704a1c364a305dda3465692461199
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-05-05 17:00:42 +0200

    Fix: send output file path

--- src/Main.java
+++ src/Main.java
@@ -19,15 +19,16 @@ import static mips.MipsGenerator.generateMipsFromFile;
 
 public class Main {
     private static final String PATH_FILE = "file.sus";
-    private static final String PATH_TAC = "tac.txt";
-    private static final String PATH_MIPS = "mips.asm";
+    private static final String PATH_TAC = "tac2.txt";
+    private static final String PATH_MIPS = "mips2.asm";
 
     public static void main(String[] args) throws InvalidTreeException, DuplicateVariableException, UnknownVariableException, IOException {
+        File tac = new File(PATH_TAC);
         Parser p = new Parser(new TokenBuffer(new CodeProcessor(PATH_FILE)), new GrammarAnalizer());
-        p.compile(new File(PATH_TAC));
+        p.compile(tac);
         //p.test(); TODO: Uncomment for final commit
         OptimizerManager om = new OptimizerManager();
-        om.optimize();
+        om.optimize(tac);
         generateMipsFromFile(PATH_TAC, PATH_MIPS);
     }
 }
