333cd60f54a884a6ae67f72da9009f3fc22a65ef
Carles <roch.carles@gmail.com>
2022-05-12 00:56:21 +0200

    MIPS boolean

--- src/mips/TacMipsAdapter.java
+++ src/mips/TacMipsAdapter.java
@@ -6,7 +6,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TacMipsAdapter {
-    private static final Pattern registerPattern = Pattern.compile("t(\\d+)");
 
     public static ArrayList<String> adaptTac(ArrayList<String> lines) {
         int[] high = findHighestRegiter(lines);
@@ -14,11 +13,45 @@ public class TacMipsAdapter {
         int[] highestLabel = {high[1]};
 
         Matcher matcher;
-        Pattern operation = Pattern.compile("(t\\d+) := (t?-?\\d+) ([+\\-*/%&|]) (t?-?\\d+)");
+        Pattern operation = Pattern.compile("(t\\d+) := (t?-?\\d+) ([+\\-*/%&|]|==|!=|<|>|>=|<=) (t?-?\\d+)");
+
         Pattern not = Pattern.compile("(.*)(!t?-?\\d+)(.*)");
+        Pattern boolIf = Pattern.compile("(.*if )(!?t?-?\\d+)( goto.*)");
+        Pattern compareIf = Pattern.compile("(.*if )(t?-?\\d+) (==|!=|<|>|>=|<=) (t?-?\\d+)( goto.*)");
 
         for (int i = 0; i < lines.size(); i++) {
             String line = lines.get(i);
+
+            // Boolean if
+            matcher = boolIf.matcher(line);
+            if (matcher.matches()) {
+                lines.remove(i);
+                String value = matcher.group(2);
+                String symbol;
+                if (value.charAt(0) == '!') {
+                    symbol = "==";
+                    value = value.substring(1);
+                } else {
+                    symbol = "!=";
+                }
+                lines.add(i, matcher.replaceFirst("$1"+value+" "+symbol+" 0$3"));
+                line = lines.get(i);
+            }
+
+            // Compare if
+            matcher = compareIf.matcher(line);
+            if (matcher.matches()) {
+                String arg1 = matcher.group(2);
+                String arg2 = matcher.group(4);
+                if (!arg1.contains("t") || !arg2.contains("t")) {
+                    ArrayList<String> newLines;
+                    newLines = doWithRegisters(matcher, highestRegister);
+                    lines.remove(i);
+                    lines.addAll(i, newLines);
+                    i += newLines.size() - 1;
+                }
+            }
+
             matcher = operation.matcher(line);
             // Per cada operació trobada
             while (matcher.find()) {
@@ -28,10 +61,11 @@ public class TacMipsAdapter {
 
                 // Logic operations
                 if (isLogicOperation(symbol)) {
-                    ArrayList<String> newLines = translateBooleanOperation(matcher, highestLabel);
+                    ArrayList<String> newLines = translateBooleanOperation(matcher, highestLabel, highestRegister);
                     lines.remove(i);
                     lines.addAll(i, newLines);
-                    i += newLines.size() - 1;
+                    i--;
+                    continue;
                 }
 
                 // Format number operations
@@ -48,66 +82,47 @@ public class TacMipsAdapter {
                     i += newLines.size() - 1;
                 }
             }
-
-            // Not
-            matcher = not.matcher(line);
-            if (matcher.matches()) {
-                ArrayList<String> newLines = new ArrayList<>();
-                int register = ++highestRegister[0];
-                int l1 = ++highestLabel[0], l2 = ++highestLabel[0];
-
-                newLines.addAll(translateBoolean(matcher, register, l1, l2, true));
-
-                newLines.add(matcher.group(1)+"t"+register+"!= 0"+matcher.group(3));
-
-                lines.remove(i);
-                lines.addAll(i, newLines);
-                i += newLines.size() - 1;
-            }
         }
         return lines;
     }
 
-    private static ArrayList<String> translateBoolean(Matcher matcher, int register, int l1, int l2, boolean hasNot) {
-        ArrayList<String> newLines = new ArrayList<>();
-        String arg = matcher.group(2).substring(1);
-        String symbol = hasNot ? "!=" : "==";
-        newLines.add("if "+arg+" "+symbol+" 0 goto L"+l1);
-        newLines.add("t"+register+" := 1");
-        newLines.add("goto L"+l2);
-        newLines.add("L"+l1+": t"+register+" := 0");
-        newLines.add("L"+l2+":");
-
-        return newLines;
-    }
-
     private static boolean isLogicOperation(String symbol) {
-        return symbol.equals("&") || symbol.equals("|");
+        return symbol.equals("&") || symbol.equals("|") || symbol.contains("=") || symbol.contains("<") || symbol.contains(">");
     }
 
-    private static ArrayList<String> translateBooleanOperation(Matcher line, int[] highestLabel) {
+    private static ArrayList<String> translateBooleanOperation(Matcher line, int[] highestLabel, int[] highestRegister) {
         String symbol = line.group(3);
         ArrayList<String> results = new ArrayList<>();
-        String assign = line.replaceFirst("$1 := 1");
+        String assign1 = line.replaceFirst("$1 := 1");
+        String assign0 = line.replaceFirst("$1 := 0");
         int label1 = ++highestLabel[0];
+        int label2 = ++highestLabel[0];
         switch (symbol) {
             case "&":
-                results.add(line.replaceFirst("if := $2 != 0 goto L" + label1));
-                results.add(line.replaceFirst("if := $4 != 0 goto L" + label1));
-                results.add(assign);
-                results.add("L"+label1+":");
+                results.add(line.replaceFirst("if $2 == 0 goto L" + label1));
+                results.add(line.replaceFirst("if $4 == 0 goto L" + label1));
+                results.add(assign1);
+                results.add("goto L"+label2);
+                results.add("L"+label1+": "+assign0);
+                results.add("L"+label2+":");
                 break;
 
             case "|":
-                int label2 = ++highestLabel[0];
-                results.add(line.replaceFirst("if := $2 != 0 goto L" + label1));
-                results.add(line.replaceFirst("if := $4 == 0 goto L" + label2));
-                results.add("L"+label1+": " + assign);
+                results.add(line.replaceFirst("if $2 != 0 goto L" + label1));
+                results.add(line.replaceFirst("if $4 == 0 goto L" + label1));
+                results.add(assign1);
+                results.add("goto L"+label2);
+                results.add("L"+label1+": "+assign0);
                 results.add("L"+label2+":");
                 break;
 
             default:
-                throw new InvalidTacException();
+                results.add(line.replaceFirst("if $2 $3 $4 goto L" + label1));
+                results.add(assign0);
+                results.add("goto L"+label2);
+                results.add("L"+label1+": "+assign1);
+                results.add("L"+label2+":");
+                break;
         }
         return results;
     }
@@ -126,7 +141,11 @@ public class TacMipsAdapter {
             arg2 = "t" + ++maxRegisters[0];
             results.add(arg2 + line.replaceFirst(" := $4"));
         }
-        results.add(line.group(1) + " := " + arg1 + " " + line.group(3) + " " + arg2);
+        if (symbol.contains("=") || symbol.contains("<") || symbol.contains(">")) {
+            results.add(line.group(1) + arg1 + " " + line.group(3) + " " + arg2 + line.group(5));
+        } else {
+            results.add(line.group(1) + " := " + arg1 + " " + line.group(3) + " " + arg2);
+        }
         return results;
     }
 
@@ -147,11 +166,19 @@ public class TacMipsAdapter {
     private static int[] findHighestRegiter(ArrayList<String> lines) {
         int highest[] = {0, 0};
         Matcher matcher;
+        Pattern registerPattern = Pattern.compile("t(\\d+)");
+        Pattern label = Pattern.compile("L(\\d+)");
+
         for (String line : lines) {
             matcher = registerPattern.matcher(line);
             while (matcher.find()) {
                 int register = Integer.parseInt(matcher.group(1));
                 if (register > highest[0]) highest[0] = register;
+            }
+            matcher = label.matcher(line);
+            while (matcher.find()) {
+                int labelNum = Integer.parseInt(matcher.group(1));
+                if (labelNum > highest[1]) highest[1] = labelNum;
             }
         }
         return highest;
