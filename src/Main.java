91e0d4d3de2ac911a6ca80fb3aa05e5a3437e0de
Pol Piñol Castuera <pol.pinol@students.salle.url.edu>
2022-05-11 09:09:39 +0200

    Priority bug at if solved.

--- src/Main.java
+++ src/Main.java
@@ -19,8 +19,8 @@ import static mips.MipsGenerator.generateMipsFromFile;
 
 public class Main {
     private static final String PATH_FILE = "file.sus";
-    private static final String PATH_TAC = "tac2.txt";
-    private static final String PATH_MIPS = "mips2.asm";
+    private static final String PATH_TAC = "tac.txt";
+    private static final String PATH_MIPS = "mips.asm";
 
     public static void main(String[] args) throws InvalidTreeException, DuplicateVariableException, UnknownVariableException, IOException {
         File tac = new File(PATH_TAC);
