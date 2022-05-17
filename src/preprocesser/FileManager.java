be25652f349358e7489a13b4c6a36ddd142465d2
Pol Piñol Castuera <pol.pinol@students.salle.url.edu>
2022-05-17 20:25:34 +0200

    Comments for preprocessor package.

--- src/preprocesser/FileManager.java
+++ src/preprocesser/FileManager.java
@@ -5,14 +5,31 @@ import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+/**
+ * Class FileManager. It only consists on read lines of the file to preprocess.
+ */
 public class FileManager {
     private final Scanner file;
 
+    /**
+     * Constructor for FileManager.
+     *
+     * @param path the path
+     * @throws FileNotFoundException the file not found exception
+     */
     public FileManager(String path) throws FileNotFoundException {
         this.file = new Scanner(new File(path));
     }
 
+    /**
+     * Read next line from file.
+     *
+     * @return next line string
+     * @throws NoSuchElementException the no such element exception
+     */
     public String readNextLine() throws NoSuchElementException {
         return this.file.nextLine();
     }
 }
+
+
--- src/preprocesser/LineRequest.java
+++ src/preprocesser/LineRequest.java
@@ -2,6 +2,15 @@ package preprocesser;
 
 import java.util.NoSuchElementException;
 
+/**
+ * The interface LineRequest.
+ */
 public interface LineRequest {
+    /**
+     * Gets next line from the file.
+     *
+     * @return next line string
+     * @throws NoSuchElementException the no such element exception
+     */
     public String getNextLine() throws NoSuchElementException;
 }
