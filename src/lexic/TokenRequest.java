f9a9e616ab6fa02c04dffc0765e56a2cd0ae11cb
Pol Piñol Castuera <pol.pinol@students.salle.url.edu>
2022-05-17 20:31:00 +0200

    Comments for lexic package.

--- src/lexic/TokenRequest.java
+++ src/lexic/TokenRequest.java
@@ -4,10 +4,42 @@ import entities.TokenDataPair;
 
 import java.util.List;
 
+/**
+ * The interface TokenRequest.
+ */
 public interface TokenRequest {
+    /**
+     * Request next token with data.
+     *
+     * @return the token data pair
+     */
     public TokenDataPair requestNextToken();
+
+    /**
+     * Gets current line.
+     *
+     * @return the current line
+     */
     public int getCurrentLine();
+
+    /**
+     * Gets current column.
+     *
+     * @return the current column
+     */
     public int getCurrentColumn();
+
+    /**
+     * Return all tokens.
+     *
+     * @param tokens list with tokens
+     */
     public void returnTokens(List<TokenDataPair> tokens);
+
+    /**
+     * Return token as data.
+     *
+     * @param token token with data
+     */
     public void returnTokens(TokenDataPair token);
 }
